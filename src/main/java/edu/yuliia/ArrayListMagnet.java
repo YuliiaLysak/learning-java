c10aeb947957331cb280594e09f9109ae87b4e14
Yuliia Lysak <julia.m.lysak@gmail.com>
2020-02-08 14:21:32 +0200

    Examples

--- src/main/java/edu/yuliia/ArrayListMagnet.java
+++ src/main/java/edu/yuliia/ArrayListMagnet.java
@@ -1,4 +1,41 @@
 package edu.yuliia;
 
+import java.util.*;
+
 public class ArrayListMagnet {
+
+    public static void printAl(ArrayList<String> al) {
+        for (String element : al) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+    }
+
+    public static void main(String args[]) {
+        ArrayList<String> a = new ArrayList<String>();
+        a.add(0, "ноль");
+        a.add(1, "один");
+        a.add(2, "два");
+        a.add(3, "три");
+        printAl(a);
+
+        a.remove(2);
+
+        if (a.contains("три")) {
+            a.add("четыре");
+        }
+        printAl(a);
+
+
+        if (a.contains("два")) {
+            a.add("2.2");
+        }
+        printAl(a);
+
+        if (a.indexOf("четыре")!= 4) {
+            a.add(4, "4.2");
+        }
+        printAl(a);
+    }
 }
+
